1da7a2b6d091e6f50f722e4a398f75ee64036f13
Christian Nuss <christian.nuss@gmail.com>
2021-09-28 10:31:02 -0700

    Escape command

--- index.js
+++ index.js
@@ -729,7 +729,7 @@ const run = async () => {
       });
       await terraformInit(organization, repo, moduleDirectory);
       console.log(`Running terraform command: \`terraform ${command}\``);
-      await exec(organization, `terraform ${command}`);
+      await exec(organization, `terraform ${escape(command)}`);
       break;
     }
 
